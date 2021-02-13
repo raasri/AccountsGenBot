59627c35925404a1a0eb8bf830dcdefd858bc058
Rahul Srivastav <69507506+raasri@users.noreply.github.com>
2021-02-13 11:40:02 +0530

    Update help.py

--- AccountGen/plugins/help.py
+++ AccountGen/plugins/help.py
@@ -16,6 +16,6 @@ async def helper(event):
     text="I am an Account Generator Bot!\nI can generate working accounts for you.\n\nClick generate accounts to get your account!! Make sure to join my channel and support me!"
     await event.edit(text,
                      buttons=[
-                         [Button.url("Channel", url=ltc), # Button.url("Dev", url="#")],
+                         [Button.url("Channel", url=ltc)],
                          [Button.inline("Generate Accounts", data="gen")]
                      ])
